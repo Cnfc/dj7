30fb5a29f561c2905ecc39fce81d52acbf7a0205
cnfc <stdashkov@mail.ru>
2018-07-01 13:27:11 +0300

    new branch and transitions

--- src/index.js
+++ src/index.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link, NavLink, Switch} from 'react-router-dom';
+import './css/index.css';
+import App from './App';
 
-// Components
-import Home from './components/home';
-import Posts from './components/posts';
-import Profile from './components/profile';
-import PostItem from './components/PostItem';
-import Life from './components/lifecycles';
-import Conditional from './components/conditional';
-import User from './components/user';
-
-
-// meawesomeapp.com/post
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <header>
-          <NavLink to="/">Home</NavLink><br/>
-          <NavLink
-             to="Posts"
-             activeStyle={{color:'red'}}
-             activeClassName="selected"
-             >Posts</NavLink><br/>
-           <NavLink to={{
-               pathname:'/profile'
-             }}>Profile</NavLink><br/>
-           <NavLink to="/Life">Life</NavLink><br/>
-           <NavLink to="/conditional">conditional</NavLink><br/>
-           <NavLink to="/user">User</NavLink><br/>
-           <hr/>
-        </header>
-        <Switch>
-          <Route path="/posts/:id/:username" component={PostItem}/>
-          <Route path="/profile" component={Profile}/>
-          <Route path="/posts" component={Posts}/>
-          <Route path="/life" component={Life}/>
-          <Route path="/conditional" component={Conditional}/>
-          <Route path="/user" component={User}/>
-          <Route path="/" exact component={Home}/>
-      </Switch>
-      </div>
-    </BrowserRouter>
-  )
-}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
